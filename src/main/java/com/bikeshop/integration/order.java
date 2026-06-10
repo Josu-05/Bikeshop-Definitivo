9d24dc6bcd9a501c1bdeeff14cdbd58d3012d972
Josu-05 <josuevillalva05@hotmail.com>
2026-06-09 22:36:20 -0500

    Create Order class with properties and constructor

--- src/main/java/com/bikeshop/integration/order.java
+++ src/main/java/com/bikeshop/integration/order.java
@@ -1,1 +1,18 @@
+package com.bikeshop.integration;
+
+public class Order {
+    private String id;
+    private String itemId;
+    private int quantity;
+
+    public Order(String id, String itemId, int quantity) {
+        this.id = id;
+        this.itemId = itemId;
+        this.quantity = quantity;
+    }
+
+    public String getId() { return id; }
+    public String getItemId() { return itemId; }
+    public int getQuantity() { return quantity; }
+}
 
