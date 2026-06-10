41f03cb3507135058fea8282f7e5fedce15e8efb
Josu-05 <josuevillalva05@hotmail.com>
2026-06-09 22:38:21 -0500

    Delete src/main/java/com/bikeshop/cart/CheckoutService.java

--- src/main/java/com/bikeshop/cart/CheckoutService.java
+++
@@ -1,9 +0,0 @@
-package com.bikeshop.cart;
-
-public class CheckoutService {
-
-    public boolean checkout(String itemId, int quantity, String cardId) {
-        // Lógica mínima: siempre devuelve true
-        return true;
-    }
-}
