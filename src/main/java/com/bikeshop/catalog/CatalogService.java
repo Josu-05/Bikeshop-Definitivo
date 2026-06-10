66d21a39ef593cb2712235f216764af9e7f93f9e
Josu-05 <josuevillalva05@hotmail.com>
2026-06-09 22:33:29 -0500

    Update CatalogService.java

--- src/main/java/com/bikeshop/catalog/CatalogService.java
+++ src/main/java/com/bikeshop/catalog/CatalogService.java
@@ -1,5 +1,5 @@
 // src/main/java/com/bikeshop/catalog/CatalogService.java
-package com.bikeshop.catalog.cart;
+package com.bikeshop.catalog;
 
 public class CatalogService {
     public Bike findById(String id) { return new Bike(id); }
