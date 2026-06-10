182972ce8af78df48da58b600dae77d6b8dd54f4
Josu-05 <josuevillalva05@hotmail.com>
2026-06-09 22:19:09 -0500

    Delete src/main/java/com/bikeshop/cart/Bike.java

--- src/main/java/com/bikeshop/cart/Bike.java
+++
@@ -1,8 +0,0 @@
-// src/main/java/com/bikeshop/catalog/Bike.java
-package com.bikeshop.cart;
-
-public class Bike {
-    private String id;
-    public Bike(String id) { this.id = id; }
-    public String getId() { return id; }
-}
