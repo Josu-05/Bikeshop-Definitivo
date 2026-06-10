13b1303f72f8b600533fae854c01969021c3710d
Josu-05 <josuevillalva05@hotmail.com>
2026-06-09 22:16:55 -0500

    Delete src/main/java/com/bikeshop/cart/CatalogService.java

--- src/main/java/com/bikeshop/cart/CatalogService.java
+++
@@ -1,6 +0,0 @@
-// src/main/java/com/bikeshop/catalog/CatalogService.java
-package com.bikeshop.catalog.cart;
-
-public class CatalogService {
-    public Bike findById(String id) { return new Bike(id); }
-}
