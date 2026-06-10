2f7715e6bef7f784230f5a2e33a3a219d7eb5023
Josu-05 <josuevillalva05@hotmail.com>
2026-06-09 22:45:04 -0500

    Update CheckoutService.java

--- src/main/java/com/bikeshop/integration/CheckoutService.java
+++ src/main/java/com/bikeshop/integration/CheckoutService.java
@@ -1,5 +1,8 @@
 package com.bikeshop.integration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CheckoutService {
     public boolean checkout(String itemId, int quantity, String cardId) {
         return true; // lógica mínima
