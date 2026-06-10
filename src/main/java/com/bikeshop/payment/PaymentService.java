9138200337ec36c5ff8819034fd2563987cfdabf
Josu-05 <josuevillalva05@hotmail.com>
2026-06-09 22:35:05 -0500

    Update PaymentService.java

--- src/main/java/com/bikeshop/payment/PaymentService.java
+++ src/main/java/com/bikeshop/payment/PaymentService.java
@@ -1,1 +1,11 @@
+package com.bikeshop.payment;
+
+public class PaymentService {
+    public boolean authorize(String cardId, double amount) {
+        return true; // lógica mínima
+    }
+    public boolean process(String cardId, double amount) {
+        return true;
+    }
+}
 
