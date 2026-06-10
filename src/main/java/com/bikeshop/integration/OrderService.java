5e9f19c6e636bebe07ebb517ac2cb91f176b43df
Josu-05 <josuevillalva05@hotmail.com>
2026-06-09 22:45:20 -0500

    Add @Service annotation to OrderService class

--- src/main/java/com/bikeshop/integration/OrderService.java
+++ src/main/java/com/bikeshop/integration/OrderService.java
@@ -1,7 +1,9 @@
 package com.bikeshop.integration;
 
+import org.springframework.stereotype.Service;
 import java.util.UUID;
 
+@Service
 public class OrderService {
     public Order createOrder(String itemId, int quantity) {
         String id = UUID.randomUUID().toString();
